d9d4391be432399aa8c9380a629c7981097a4b60
Sergey Redko <sergeyredjko@gmail.com>
2015-01-22 00:38:27 +0300

    Add Logger

--- src/main/java/com/autotesting/framework/screens/MailBaseScreen.java
+++ src/main/java/com/autotesting/framework/screens/MailBaseScreen.java
@@ -1,12 +1,15 @@
 package com.autotesting.framework.screens;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.log4j.Logger;
 
 public class MailBaseScreen {
+
+    protected static final Logger log = Logger.getLogger(WebDriver.class);
+
     public MailBaseScreen mailCloseBrowser(WebDriver webDriver)
     {
+        log.info("Закрываем браузер");
         webDriver.quit();
         return this;
     }
--- src/main/java/com/autotesting/framework/screens/MailLoginPageScreen.java
+++ src/main/java/com/autotesting/framework/screens/MailLoginPageScreen.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class MailLoginPageScreen {
+public class MailLoginPageScreen extends MailBaseScreen{
     private final String MAIL_URL_FOR_TEST = "https://mail.ru";
     private final String EXPECTED_USER_EMAIL_XPATH = "//i[@id='PH_user-email']";
     private final String USER_NAME_FOR_LOGIN = "auto_test_user01";
@@ -15,17 +15,20 @@ public class MailLoginPageScreen {
     private static final String LOGIN_PAGE_BUTTON_XPATH = "//input[@id='mailbox__auth__button']";
 
     public MailLoginPageScreen openLoginPageScreen(WebDriver webDriver) {
+        log.info(String.format("Открываем в браузере страницу логина по адресу %s", MAIL_URL_FOR_TEST));
         webDriver.get(MAIL_URL_FOR_TEST);
         return this;
     }
 
     public String getUserEmailText(WebDriver webDriver) {
+        log.info(String.format("Получаем значение имени пользователя из хедера страницы логина: '%s'", USER_NAME_FOR_LOGIN));
         String result = webDriver.findElement(By.xpath(EXPECTED_USER_EMAIL_XPATH)).getText();
         return result;
     }
 
     public MailLoginPageScreen inputLogin(WebElement element, WebDriver driver)
     {
+        log.info(String.format("Вводим логин: '%s'", USER_NAME_FOR_LOGIN));
         element = driver.findElement(By.xpath(LOGIN_PAGE_LOGIN_FIELD_XPATH));
         element.sendKeys(USER_NAME_FOR_LOGIN);
         return this;
@@ -33,6 +36,7 @@ public class MailLoginPageScreen {
 
     public MailLoginPageScreen inputPassword(WebElement element, WebDriver driver)
     {
+        log.info(String.format("Вводим пароль: '%s'", PASSWORD_FOR_LOGIN));
         element = driver.findElement(By.xpath(LOGIN_PAGE_PASSWORD_FIELD_XPATH));
         element.sendKeys(PASSWORD_FOR_LOGIN);
         return this;
@@ -40,6 +44,7 @@ public class MailLoginPageScreen {
 
     public MailLoginPageScreen submitAuthorizationMailForm(WebElement element, WebDriver driver)
     {
+        log.info(String.format("Отправляем форму кликом по кнопке Войти"));
         element = driver.findElement(By.xpath(LOGIN_PAGE_BUTTON_XPATH));
         element.click();
         return this;
