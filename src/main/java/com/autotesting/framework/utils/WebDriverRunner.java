4bd4e4de55d8ce369fec5133f2b5eeab5157b554
Sergey Redko <sergeyredjko@gmail.com>
2015-02-22 14:28:42 +0300

    Selenium Grid

--- src/main/java/com/autotesting/framework/utils/WebDriverRunner.java
+++ src/main/java/com/autotesting/framework/utils/WebDriverRunner.java
@@ -1,14 +1,21 @@
 package com.autotesting.framework.utils;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class WebDriverRunner {
+import java.net.URL;
+
+public class WebDriverRunner{
     private static WebDriverWrapper driver;
     private static final Logger log = Logger.getLogger(WebDriverRunner.class);
 
+
     public WebDriverRunner() {
         try {
-            driver = new WebDriverWrapper();
+            DesiredCapabilities capability = DesiredCapabilities.firefox();
+            capability.setCapability("platform", Platform.WINDOWS);
+            driver = new WebDriverWrapper(new URL("http://localhost:4444/wd/hub"), capability);
         } catch (Exception e) {
             log.error("Ошибка при создании драйвера");
         }
--- src/main/java/com/autotesting/framework/utils/WebDriverWrapper.java
+++ src/main/java/com/autotesting/framework/utils/WebDriverWrapper.java
@@ -1,16 +1,23 @@
 package com.autotesting.framework.utils;
 
+import org.openqa.selenium.Capabilities;
 import com.autotesting.framework.screens.MailBaseScreen;
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.log4j.Logger;
 
-public class WebDriverWrapper extends FirefoxDriver{
+import java.net.URL;
+
+public class WebDriverWrapper extends RemoteWebDriver{
 
     public static final int TIMEOUT_FOR_ACTION_SECONDS = 3;
     protected final Logger log = Logger.getLogger(MailBaseScreen.class);
+
+    public WebDriverWrapper(URL remoteUrl, Capabilities capabilities){
+        super(remoteUrl, capabilities);
+    }
 
     public void enterTextByXpath(String xpath, String text){
         log.info(String.format("[ACTION]: Ввод текста '%s' в элемент: '%s'", text, xpath));
